f28ca75d1909616e3f42aa702bfe2204c9c1008d
JuanMorello506 <morellojuan992@gmail.com>
2024-06-08 20:05:44 -0300

    Add models relations

--- src/Models/models.js
+++ src/Models/models.js
@@ -1,6 +1,33 @@
 import User from "./User.js";
+import Movie from "./Movie.js";
+import Comment from "./Comment.js";
+import Category from "./Category.js";
+
+
+Movie.belongsToMany(Category, { through: 'MovieCategories', foreignKey: "categoryId" });
+Category.belongsToMany(Movie, { through: 'MovieCategories', foreignKey: 'movieId' });
+
+
+Movie.hasMany(Category,{
+    foreignKey: "movieId"
+})
+
+Movie.hasMany(Comment,{
+    foreignKey: "movieId"
+})
+
+Movie.belongsToMany(User,{ through: 'Favourites', foreignKey: "userId"})
+User.belongsToMany(Movie, {through: 'Favourites', foreignKey: "movieId"})
+
+
+Comment.belongsTo(Movie,{
+    foreignKey: "movieId"
+})
+
+Comment.belongsTo(User,{
+    foreignKey: "userId"
+})
 
 
 
-
-export { User };
+export { User, Movie, Comment, Category };
--- src/seed/roleSeed.js
+++
@@ -1,6 +0,0 @@
-import { Role } from "../Models/models.js";
-
-export const roleSeed = async () =>
-  await Role.bulkCreate([{ name: "admin" }, { name: "user" }]);
-
-await roleSeed();

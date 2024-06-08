4df2ebd8357de0e4207dcf2ebdf6fa445715ca19
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2024-06-08 20:40:50 -0300

    corregir

--- src/Models/models.js
+++ src/Models/models.js
@@ -3,31 +3,23 @@ import Movie from "./Movie.js";
 import Comment from "./Comment.js";
 import Category from "./Category.js";
 
-
-Movie.belongsToMany(Category, { through: 'MovieCategories', foreignKey: "categoryId" });
-Category.belongsToMany(Movie, { through: 'MovieCategories', foreignKey: 'movieId' });
-
-
-Movie.hasMany(Category,{
-    foreignKey: "movieId"
+Category.belongsTo(Movie, {
+	foreignKey: "movieId"
 })
 
-Movie.hasMany(Comment,{
-    foreignKey: "movieId"
+Movie.hasMany(Comment, {
+	foreignKey: "movieId"
 })
 
-Movie.belongsToMany(User,{ through: 'Favourites', foreignKey: "userId"})
-User.belongsToMany(Movie, {through: 'Favourites', foreignKey: "movieId"})
+Movie.belongsToMany(User, { through: 'Favourites', foreignKey: "userId" })
+User.belongsToMany(Movie, { through: 'Favourites', foreignKey: "movieId" })
 
-
-Comment.belongsTo(Movie,{
-    foreignKey: "movieId"
+Comment.belongsTo(Movie, {
+	foreignKey: "movieId"
 })
 
-Comment.belongsTo(User,{
-    foreignKey: "userId"
+Comment.belongsTo(User, {
+	foreignKey: "userId"
 })
-
-
 
 export { User, Movie, Comment, Category };

a47b6203bfcfe30dfd6b331d4065aca7cdf62bf9
samefede <federico.farinia@gmail.com>
2024-06-08 19:04:49 -0300

    delete role

--- src/Models/models.js
+++ src/Models/models.js
@@ -1,11 +1,6 @@
-import Role from "./Role.js";
 import User from "./User.js";
 
-Role.hasMany(User, {
-  foreignKey: "roleId",
-});
-User.belongsTo(Role, {
-  foreignKey: "roleId",
-});
 
-export { Role, User };
+
+
+export { User };

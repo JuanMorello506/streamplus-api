69015381576618d025a0fb5432c9c74e8868f4bb
samefede <federico.farinia@gmail.com>
2024-06-09 18:27:50 -0300

    routes

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { validateLogin } from "../middlewares/validateLogin.js";
+import CategoryControllers from "../Controllers/CategoryController.js";
+
+const categoryControllers = new CategoryControllers();
+
+const categoryRoutes = Router();
+
+categoryRoutes.use(validateLogin);
+categoryRoutes.get("/", categoryControllers.getAllCategories);
+categoryRoutes.get("/:id", categoryControllers.getCategoryById);
+categoryRoutes.put("/:id", categoryControllers.updateCategory);
+categoryRoutes.delete("/:id", categoryControllers.deleteCategory);
+
+export default categoryRoutes;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRoutes from "./userRoutes.js";
 import postRoutes from "./postRoutes.js";
 import { logger } from "../middlewares/middlewares.js";
+import categoryRoutes from "./categoryRoutes.js";
 
 const routes = Router();
 
 
-routes.use("/user", logger, userRoutes);
-routes.use("/post", postRoutes);
+routes.use("streamplus/user", logger, userRoutes);
+routes.use("streamplus/post", postRoutes);
+routes.use("streamplus/categories", logger, categoryRoutes);
 
 export default routes;
